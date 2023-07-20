a03763cd208c54b2ca55ac7116cf17210da59d4a
Ouzzwalid <ouzz.walid@gmail.com>
2023-07-20 13:07:04 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	long i, n=612852475143;
-	
+	long i, n = 612852475143;
+
 	for (i = 2; i <= n; i++)
 	{
 		if ((n % i) == 0)
@@ -19,6 +19,6 @@ int main(void)
 		}
 
 	}
-	printf("%lu\n",i);
+	printf("%lu\n", i);
 	return (0);
 }
