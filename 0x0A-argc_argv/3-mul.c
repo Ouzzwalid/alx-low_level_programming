d35b36da6599ead969fc578e6a478c0b4e63d4b6
Ouzzwalid <ouzz.walid@gmail.com>
2023-08-04 16:09:11 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2])); 
+	return (0);
+}
