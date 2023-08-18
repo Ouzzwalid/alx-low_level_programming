0f24e26f95643ba8e8797fa8b1ffa8ba22232df3
Ouzzwalid <ouzz.walid@gmail.com>
2023-08-18 10:59:57 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,14 @@
 #ifndef HEADER
 #define HEADER
 /**
- *
- *
- *
- *
+ *struct dog - define a new structure called dog
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner : the owner of the dog
  */
 struct dog
 {
 	char *name;
 	float age;
-	char * owner;
+	char *owner;
 }
