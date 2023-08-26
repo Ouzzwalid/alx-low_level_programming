6d0cceac181ff1d7cf19b9f3e4c72eb3ab119b58
Ouzzwalid <ouzz.walid@gmail.com>
2023-08-26 14:57:05 -0700

    Task 0 error fix"

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,16 +12,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t i;
-
-	for (i = 0; h != NULL; i++)
+	
+	i = 0;
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
