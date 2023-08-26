9599a0998113810f01a33c3986a2ebe0484ce5fd
Ouzzwalid <ouzz.walid@gmail.com>
2023-08-26 15:54:15 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
