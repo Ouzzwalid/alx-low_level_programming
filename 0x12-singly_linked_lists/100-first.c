cc9403dd7f8938b4b081099916adbff0f9fc040d
Ouzzwalid <ouzz.walid@gmail.com>
2023-08-26 16:15:30 -0700

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void print_before_main(void) __attribute__((constructor));
+/**
+ *
+ *
+ *
+ *
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
