17203d812ef334c8cb1e4697391956fb04998e21
Walid ouzzane <ouzz.walid@gmail.com>
2023-10-08 08:40:23 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints the number if arguments passed to it
+ *
+ *@argc : the number of arguments including the program name
+ *@argv : array of arfgs
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
