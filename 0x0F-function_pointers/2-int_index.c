0e6b7dd2bb33d928e9812b0bfffcea20cc8e250e
Walid ouzzane <ouzz.walid@gmail.com>
2023-10-11 07:18:55 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ *int_index - Search for an indew that matches to a function return
+ *
+ *@array: pointer to array
+ *@size: the size of the array
+ *@cmp: pointer to function to be executed to array elements
+ *
+ *Return: index of the first element for which the function doesn't return 0
+ * -1 otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size > 0 && array != NULL && cmp != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+		return (-1);
+	}
+	return (-1);
+}
