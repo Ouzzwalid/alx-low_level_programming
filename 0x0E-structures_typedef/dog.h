ffbc3a44d0747bb7fb72d8c6bdd8d11a6bad9f66
Walid ouzzane <ouzz.walid@gmail.com>
2023-10-11 05:36:31 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 #ifndef HEADERFILE
 #define HEADERFILE dog.h
 /**
- *
- *
- *
- *
+ *struct dog - structur that defines a dog
+ *@name: The name of the dog
+ *@age: the age of the dog
+ *@owner: The owner of the dog
  */
 struct dog
 {
